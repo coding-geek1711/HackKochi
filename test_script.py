ee953a9b2b91b4586492c63873001c9b658ed31e
coding-geek1711 <mahestpm@gmail.com>
2020-11-08 08:43:49 +0530

    Update Readme

--- test_script.py
+++ test_script.py
@@ -1,10 +1,20 @@
 import cv2
-from CreateMaskForImage import drawBoundingRects
+import numpy as np
+# from CreateMaskForImage import drawBoundingRects
 
-img = cv2.imread("temp/20201107_135456.jpg")
-x, y, w, h = 511, 583, 11, 20
+image = cv2.imread("UploadImages/test_image.jpg")
+image = cv2.resize(image, (640, 640))
 
-cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
-cv2.imshow("IMAGE", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+gray_scale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+_, mask = cv2.threshold(gray_scale, 170, 255, cv2.THRESH_BINARY)
+
+kernel = np.ones((5, 5), np.uint8)
+eroded_mask = cv2.erode(mask, kernel)
+
+
+contours, heirarchy = cv2.findContours(
+    eroded_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+areas = [cv2.contourArea(c) for c in contours]
+
